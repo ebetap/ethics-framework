ccd82f6fd6e7bdbe76468b97c06661f92fc4cb48
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-27 16:22:44 +0700

    Update ComplianceMonitor.js

--- src/models/ComplianceMonitor.js
+++ src/models/ComplianceMonitor.js
@@ -1,1 +1,12 @@
+class ComplianceMonitor {
+    constructor(rules) {
+        this.rules = rules;
+    }
 
+    checkCompliance(data) {
+        console.log("Checking compliance...");
+        return this.rules.every(rule => rule.check(data));
+    }
+}
+
+export default ComplianceMonitor;
