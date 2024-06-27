33561636e9e48c6aef6018230450149fe529a33a
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-27 16:21:57 +0700

    Update BiasDetection.js

--- src/models/BiasDetection.js
+++ src/models/BiasDetection.js
@@ -1,1 +1,13 @@
+class BiasDetection {
+    detectBias(data) {
+        console.log("Detecting bias...");
+        return data.filter(item => item.includes("bias"));
+    }
 
+    mitigateBias(data) {
+        console.log("Mitigating bias...");
+        return data.map(item => item.replace("bias", "mitigated"));
+    }
+}
+
+export default BiasDetection;
