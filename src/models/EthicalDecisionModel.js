4c5745fbe5626cb05052df02f03c6d4204686a79
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-27 16:21:06 +0700

    Update EthicalDecisionModel.js

--- src/models/EthicalDecisionModel.js
+++ src/models/EthicalDecisionModel.js
@@ -1,1 +1,23 @@
+import * as tf from '@tensorflow/tfjs';
 
+class EthicalDecisionModel {
+    constructor() {
+        this.model = tf.sequential();
+        this.model.add(tf.layers.dense({ units: 16, activation: 'relu', inputShape: [10] }));
+        this.model.add(tf.layers.dense({ units: 1, activation: 'sigmoid' }));
+        this.model.compile({ optimizer: 'adam', loss: 'binaryCrossentropy', metrics: ['accuracy'] });
+    }
+
+    async train(data, labels) {
+        const xs = tf.tensor2d(data);
+        const ys = tf.tensor2d(labels);
+        await this.model.fit(xs, ys, { epochs: 10 });
+    }
+
+    predict(data) {
+        const xs = tf.tensor2d(data);
+        return this.model.predict(xs).arraySync();
+    }
+}
+
+export default EthicalDecisionModel;
