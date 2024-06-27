29ffa679a305f48a95e556ceb51fb2c62a4a317b
Beta Priyoko <beta.priyoko@students.amikom.ac.id>
2024-06-27 16:25:00 +0700

    Update FeatureExtractor.js

--- src/models/FeatureExtractor.js
+++ src/models/FeatureExtractor.js
@@ -1,1 +1,20 @@
+import * as tf from '@tensorflow/tfjs';
 
+class FeatureExtractor {
+    constructor() {
+        this.model = tf.sequential();
+        this.model.add(tf.layers.dense({ units: 16, activation: 'relu', inputShape: [10] }));
+        this.model.add(tf.layers.dense({ units: 8, activation: 'relu' }));
+        this.model.add(tf.layers.dense({ units: 16, activation: 'relu' }));
+        this.model.add(tf.layers.dense({ units: 10, activation: 'sigmoid' }));
+        this.model.compile({ optimizer: 'adam', loss: 'meanSquaredError' });
+    }
+
+    async extractFeatures(data) {
+        const xs = tf.tensor2d(data);
+        const features = await this.model.predict(xs).arraySync();
+        return features;
+    }
+}
+
+export default FeatureExtractor;
